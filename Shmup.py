062982626acadb5bcfab07a7f5f27e98868cc25b
johnson062 <johnson062@hotmail.com>
2018-05-07 11:26:13 -0400

    updated

--- Shmup.py
+++ Shmup.py
@@ -16,6 +16,7 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
+
 # Set up assets folders
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "img")
@@ -29,6 +30,16 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Shmug")
 # Define clock
 clock = pygame.time.Clock()
+
+font_name = pygame.font.match_font('arial')
+
+
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
 
 
 class Player(pygame.sprite.Sprite):
@@ -68,8 +79,9 @@ class Player(pygame.sprite.Sprite):
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = mob_img
-        self.image.set_colorkey(BLACK)
+        self.image_orig = random.choice(meteor_images)
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
         self.radius = int(self.rect.width*.85/2)
         #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
@@ -77,14 +89,30 @@ class Mob(pygame.sprite.Sprite):
         self.rect.y = random.randrange(-100, -40)
         self.speedy = random.randrange(1, 5)
         self.speedx = random.randrange(-3, 3)
+        # Rotate the Mob
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
+
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
 
     def update(self):
+        self.rotate()
         self.rect.y += self.speedy
         self.rect.x += self.speedx
         # Re-draw the Mob
         if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 30:
             self.rect.x = random.randrange(WIDTH - self.rect.width)
-            self.rect.y = random.randrange(-100, -40)
+            self.rect.y = random.randrange(-150, -100)
             self.speedy = random.randrange(1, 8)
             self.speedx = random.randrange(1, 3)
 
@@ -110,8 +138,13 @@ class Bullet(pygame.sprite.Sprite):
 background = pygame.image.load(os.path.join(img_folder, "bg.png")).convert()
 background_rect = background.get_rect()
 player_img = pygame.image.load(os.path.join(img_folder, "ship.png")).convert()
-mob_img = pygame.image.load(os.path.join(img_folder, "meteor.png")).convert()
+#mob_img = pygame.image.load(os.path.join(img_folder, "meteor.png")).convert()
 bullet_img = pygame.image.load(os.path.join(img_folder, "laser.png")).convert()
+meteor_images = []
+meteor_list = ['meteor1.png', 'meteor2.png', 'meteor3.png', 'meteor4.png']
+for img in meteor_list:
+    meteor_images.append(pygame.image.load(os.path.join(img_folder, img)).convert())
+
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
@@ -123,6 +156,9 @@ for i in range(8):
     m = Mob()
     all_sprites.add(mobs)
     mobs.add(m)
+
+# Score
+score = 0
 
 # Game main loop
 running = True
@@ -143,6 +179,7 @@ while running:
     # Check for collisions [Bullets]
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
     for hit in hits:
+        score += 50 - hit.radius
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
@@ -155,6 +192,8 @@ while running:
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
+    draw_text(screen, str(score), 18, WIDTH/2, 10)
+
     # after drawing everything, flip the display
     pygame.display.flip()
 
